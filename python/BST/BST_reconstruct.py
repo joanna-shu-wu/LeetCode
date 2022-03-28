d700136c4ab7e89bd4e3802469dd3dc81e0fc2d0
joanna-shu-wu <joannawu0624@gmail.com>
2022-03-27 22:38:50 -0500

    add BST_reconstruct.py

---
+++ python/BST/BST_reconstruct.py
@@ -0,0 +1,15 @@
+'''
+Write a function that take in a BST and returns the sum of its node depth
+
+Initial condition: 
+    Give a value, it is the root with 2 pointers, left pointer and right pointer
+'''
+
+
+#------------------------------------------------------------------------------------------------------------------------------------------------+
+# Hint: use recursion to treat every node as the root of the subtree
+# Step 1: pass the root of BST to the recursion function
+# Step 2: in the recursion function, find the right child-node. It means to find the index of the value which is greater than the root-node
+    # it means all the values to the right are belong to the left subtree of the node
+# Step 3: 
+#------------------------------------------------------------------------------------------------------------------------------------------------+
