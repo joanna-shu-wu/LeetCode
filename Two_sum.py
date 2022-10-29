c3229ccc3a34ac5cde330b5d2bb913b6d1f42ecd
joanna-shu-wu <joannawu0624@gmail.com>
2022-10-29 14:21:06 -0500

    add

---
+++ Two_sum.py
@@ -0,0 +1,9 @@
+#Given an array of integer, return the indices of the two numbers that add up to a given target
+
+'''Step 1: Verify the constraints:
+(1) Are all numbers positive?
+(2) Can the number has duplicate?
+(3) will there be solution available?
+(4) What to return if there no solution available?'''
+
+
