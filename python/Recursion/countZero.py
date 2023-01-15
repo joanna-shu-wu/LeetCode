402d507800d8e570506f26d3c388da37f350e6a4
joanna-shu-wu <joannawu0624@gmail.com>
2023-01-15 13:08:36 -0600

    modify countZero

--- python/Recursion/countZero.py
+++ python/Recursion/countZero.py
@@ -1,6 +1,7 @@
 '''Given a number, check if there's any zero'''
 '''hint:check remainder'''
 '''If remainder equal to 0, increase the count and reduce the number by dividing it by 10'''
+import math
 
 # Method 1: put the count inside the argument
 def countZero(number):
@@ -8,11 +9,11 @@ def countZero(number):
     return helper(number,count)
 
 def helper(number,count):
-    if number==0:
+    if math.floor(number)==0:
         return count
 
     rem=number%10
-    if rem==0:
+    if math.floor(rem)==0:
         return helper(number/10,count+1)
     return helper(number/10,count)
 
