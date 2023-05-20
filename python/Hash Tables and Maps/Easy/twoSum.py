e667000c01b9f1a91a66db40c575f9d17c73d155
joanna-shu-wu <joannawu0624@gmail.com>
2023-05-20 15:23:02 -0500

    new leetcode

--- python/Hash Tables and Maps/Easy/twoSum.py
+++ python/Hash Tables and Maps/Easy/twoSum.py
@@ -14,11 +14,20 @@ You can return the answer in any order.
 # x+y= target; y=current number, target=input => x=target-y
 # If we've seen x before, that means we just need to return the indice of x and y
 
+from typing import List
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         m={}
         for i in range(len(nums)):
-            x=target-nums[i] #nums[i] is the current element in the loop
-            if(x in m):#check if the goal is a key in the map
+            x=target-nums[i] #nums[i] is the current value in the loop
+            if x in m:#check if the goal is a key in the map
                 return [m[x],i]
-            m[nums[i]]=i #set the value of current element to the current index in the map for later iteration
+            m[nums[i]]=i #assing the key as current element and assign the as value as current index for later iteration
+
+sol=Solution()
+inputArray=[1,2,3,4]
+target=4
+result=sol.twoSum(inputArray,target)
+print(result)
+
