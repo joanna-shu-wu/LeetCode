9dd8ab5e1ae003f3e43bfd90bd4f1d300d9610b5
Joanna Wu <joannawu0624@gmail.com>
2023-07-16 07:10:58 -0500

    adding new files (#48)

--- python/Slide Window/longestStringWODuplicates.py
+++ python/Slide Window/longestStringWODuplicates.py
@@ -6,7 +6,7 @@ def longestStringWODuplicates(s):
     l=0 # initial the beginning of the window,
     res=0 #initial result
 
-    for r in range(len(s)):
+    for r in range(len(s)): #update the pointer to a new location
         while s[r] in charSet:
             charSet.remove(s[l])
             l+=1
